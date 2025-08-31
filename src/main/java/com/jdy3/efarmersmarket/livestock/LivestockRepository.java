e7d93e284c6711a1cceca4c68f180675997f0f5b
Judith Youziel <38296075+jdy3@users.noreply.github.com>
2025-08-31 11:50:43 +0000

    Add custom methods in Livestock Repository class

--- src/main/java/com/jdy3/efarmersmarket/livestock/LivestockRepository.java
+++ src/main/java/com/jdy3/efarmersmarket/livestock/LivestockRepository.java
@@ -1,9 +1,12 @@
 package com.jdy3.efarmersmarket.livestock;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface LivestockRepository extends ListCrudRepository<Livestock, UUID>{
 
+    public List<Livestock> findByName(String name);
+    public List<Livestock> findByProvenance(String farm);
 
 }
