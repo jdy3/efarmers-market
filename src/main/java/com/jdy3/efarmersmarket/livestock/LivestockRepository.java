0806f835a26530655e7aa36938fb3da370e75dc3
Judith Youziel <38296075+jdy3@users.noreply.github.com>
2025-08-31 11:46:03 +0000

    Extend Livestock repository as an interface of ListCrudRepository interface

--- src/main/java/com/jdy3/efarmersmarket/livestock/LivestockRepository.java
+++ src/main/java/com/jdy3/efarmersmarket/livestock/LivestockRepository.java
@@ -1,5 +1,9 @@
 package com.jdy3.efarmersmarket.livestock;
 
-public class LivestockRepository {
+import java.util.UUID;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface LivestockRepository extends ListCrudRepository<Livestock, UUID>{
+
 
 }
