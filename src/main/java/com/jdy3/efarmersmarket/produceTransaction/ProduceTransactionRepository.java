4e2f3976cc6e1088d7f53328e6be57d21fdee73f
Judith Youziel <38296075+jdy3@users.noreply.github.com>
2025-09-06 18:27:09 +0000

    Create produce transaction repository

--- src/main/java/com/jdy3/efarmersmarket/produceTransaction/ProduceTransactionRepository.java
+++ src/main/java/com/jdy3/efarmersmarket/produceTransaction/ProduceTransactionRepository.java
@@ -1,5 +1,19 @@
 package com.jdy3.efarmersmarket.produceTransaction;
 
-public class ProduceTransactionRepository {
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface ProduceTransactionRepository extends ListCrudRepository<ProduceTransaction, Long> {
+
+    public List<ProduceTransaction> findByProductId(UUID productId);
+
+    @Query("SELECT i FROM ProduceTransaction i WHERE i.cost > (SELECT AVG(j.cost) FROM ProduceTransaction j)")
+    public List<ProduceTransaction> findHighValueProduceTransactions();
+
+    @Query("SELECT i FROM ProduceTransaction i WHERE i.cost < (SELECT AVG(j.cost) FROM ProduceTransaction j)")
+    public List<ProduceTransaction> findLowValueProduceTransactions();
 
 }
